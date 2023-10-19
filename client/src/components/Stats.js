2c95628e70399262b9a297988fdca95c1c188dc1
Firangiz Ganbarli <65179277+firangizg@users.noreply.github.com>
2023-10-19 13:09:50 -0500

    show top genres in frontend

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -7,18 +7,34 @@ import Tracks from './Tracks.js';
 import Artists from './Artists.js';
 
 class Stats extends Component {
+  state = {
+    accessToken: null,
+  };
+
+  componentDidMount() {
+    // Grabbing the token from URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const accessToken = urlParams.get('access_token');
+
+    if (accessToken) {
+      this.setState({ accessToken });
+    }
+  }
+
   render() {
+    const { accessToken } = this.state;
+
     return (
       <div className="stats-container" id="stats">
         <h2>Statistics</h2>
-        <Genres />
+        {accessToken ? <Genres accessToken={accessToken} /> : <div>Loading genres...</div>}
         <Characteristics />
         <div className="list-stats-container">
           <Tracks />
           <Artists />
         </div>
       </div>
-    )
+    );
   }
 }
 
