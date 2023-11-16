836071b608625965bdb8bd3e73535e1124fa1e55
emmy-rich <emmy.richardson339@gmail.com>
2023-11-16 12:34:21 -0600

    Add comments.

--- movieDB/index.js
+++ movieDB/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+//get database credentials from .env file
 const credentials = {
     user: process.env.DB_USER,
     host: process.env.HOST,
@@ -11,30 +12,30 @@ const credentials = {
     port: process.env.PORT,
 };
 
-// Connect with a connection pool.
-
+// Connect to the database with a connection pool.
 async function poolDemo() {
     const pool = new Pool(credentials);
+    //nothing in the table for now so we will get the time to show connection
     const now = await pool.query("SELECT NOW()");
     await pool.end();
 
     return now;
 }
 
-// Connect with a client.
-
+// Connect to the database with a client.
 async function clientDemo() {
     const client = new Client(credentials);
     await client.connect();
+    //nothing in the table for now so we will get the time to show connection
     const now = await client.query("SELECT NOW()");
     await client.end();
 
     return now;
 }
 
-// Use a self-calling function so we can use async / await.
-
+// Use a self-calling function, so we can use async / await.
 (async () => {
+    //print the results of both
     const poolResult = await poolDemo();
     console.log("Time with pool: " + poolResult.rows[0]["now"]);
 
